15c3e7fadec4d344a7b94a76787b3706e1f66aab
HydroponicHelp <61904114+HydroponicHelp@users.noreply.github.com>
2020-03-07 15:39:52 +0300

    Add files via upload

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,23 +1,28 @@
-const CACHE = "pwabuilder-offline-page";
+// This is the "Offline copy of pages" service worker
 
+const CACHE = "pwabuilder-offline";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "index.html";
 const offlineFallbackPage = "index.html";
 
+// Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener("install", function (event) {
   console.log("[PWA Builder] Install Event processing");
 
   event.waitUntil(
     caches.open(CACHE).then(function (cache) {
       console.log("[PWA Builder] Cached offline page during install");
-      
+
       if (offlineFallbackPage === "index.html") {
         return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
       }
-      
+
       return cache.add(offlineFallbackPage);
     })
   );
 });
 
+// If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener("fetch", function (event) {
   if (event.request.method !== "GET") return;
 
@@ -26,6 +31,7 @@ self.addEventListener("fetch", function (event) {
       .then(function (response) {
         console.log("[PWA Builder] add page to offline cache: " + response.url);
 
+        // If request was success, add or update it in the cache
         event.waitUntil(updateCache(event.request, response.clone()));
 
         return response;
@@ -38,14 +44,13 @@ self.addEventListener("fetch", function (event) {
 });
 
 function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return error page
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
       if (!matching || matching.status === 404) {
-        if (request.destination !== "document" || request.mode !== "navigate") {
-          return Promise.reject("no-match");
-        }
-
-        return cache.match(offlineFallbackPage);
+        return Promise.reject("no-match");
       }
 
       return matching;
